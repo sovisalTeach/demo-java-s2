f122de4d39a87ac7894432e830073c60be1e5313
sovisalchenda <sovisal.chenda@techostartup.center>
2024-03-18 14:33:32 +0700

    add event

--- src/TaskTwo/LogIn.java
+++ src/TaskTwo/LogIn.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class LogIn implements ActionListener {
     JFrame f;
@@ -13,6 +14,10 @@ public class LogIn implements ActionListener {
     JPasswordField tPassword, tPassword2, tRepassword;
     JPanel pMain, pLogin, pLoginCenter, pSign, pSignCenter, pCorrect, pIncorrect;
     CardLayout cl;
+
+    int attempt = 3;
+
+    String varUsername, varPassword;
 
     public LogIn(){
         f = new JFrame("Username Login");
@@ -25,8 +30,8 @@ public class LogIn implements ActionListener {
         pLoginCenter = new JPanel(new GridLayout(4,2));
         pSign = new JPanel(new BorderLayout(10,10));
         pSignCenter = new JPanel(new GridLayout(4,2));
-        pCorrect = new JPanel(new FlowLayout());
-        pIncorrect = new JPanel(new FlowLayout());
+        pCorrect = new JPanel(new BorderLayout(10,10));
+        pIncorrect = new JPanel(new BorderLayout(10,10));
         lSiginTitle = new JLabel("Sign In");
         lSiginTitle.setHorizontalAlignment(JLabel.CENTER);
         lSiginTitle.setVerticalAlignment(JLabel.CENTER);
@@ -51,10 +56,26 @@ public class LogIn implements ActionListener {
         lYourAttempt = new JLabel("Your Attempts Have Only:");
         lYourAttempt.setVerticalAlignment(JLabel.CENTER);
         lAttempt = new JLabel();
+        lAttempt.setText(String.valueOf(attempt));
         lAttempt.setHorizontalAlignment(JLabel.CENTER);
         lAttempt.setVerticalAlignment(JLabel.CENTER);
-        lCorrect = new JLabel("You have used all of three attempts!!!");
-        lIncorrect = new JLabel("Welcome to Our Application");
+        ImageIcon imgCorrect = new ImageIcon("assets/correct.png");
+        lCorrect = new JLabel("Welcome to Our Application");
+        lCorrect.setIcon(imgCorrect);
+        lCorrect.setVerticalTextPosition(JLabel.TOP);
+        lCorrect.setHorizontalTextPosition(JLabel.CENTER);
+        lCorrect.setIconTextGap(15);
+        lCorrect.setHorizontalAlignment(JLabel.CENTER);
+        lCorrect.setVerticalAlignment(JLabel.CENTER);
+        ImageIcon imgIncorrect = new ImageIcon("assets/incorrect.png");
+        lIncorrect = new JLabel("You have used all of three attempts!!!");
+        lIncorrect.setIcon(imgIncorrect);
+        lIncorrect.setVerticalTextPosition(JLabel.TOP);
+        lIncorrect.setHorizontalTextPosition(JLabel.CENTER);
+        lIncorrect.setIconTextGap(15);
+        lIncorrect.setHorizontalAlignment(JLabel.CENTER);
+        lIncorrect.setVerticalAlignment(JLabel.CENTER);
+
         tUserName1 = new JTextField();
         tUserName2 = new JTextField();
         tPassword = new JPasswordField();
@@ -67,6 +88,8 @@ public class LogIn implements ActionListener {
 
         btnSignUp.addActionListener(this);
         btnSignIn.addActionListener(this);
+        btnClear.addActionListener(this);
+        btnClear2.addActionListener(this);
 
         pSignCenter.add(lUsername);
         pSignCenter.add(tUserName1);
@@ -92,9 +115,9 @@ public class LogIn implements ActionListener {
         pLogin.add(lSiginTitle,BorderLayout.NORTH);
         pLogin.add(pLoginCenter,BorderLayout.CENTER);
 
-        pCorrect.add(lCorrect);
+        pCorrect.add(lCorrect, BorderLayout.CENTER);
 
-        pIncorrect.add(lIncorrect);
+        pIncorrect.add(lIncorrect, BorderLayout.CENTER);
 
         pMain.add(pSign,"SignUp");
         pMain.add(pLogin,"LogIn");
@@ -110,10 +133,38 @@ public class LogIn implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==btnSignUp){
-            cl.next(pMain);
-        }
-        if (e.getSource()==btnSignIn) {
-            cl.next(pMain);
+            String pwdTemp = Arrays.toString(tPassword.getPassword());
+            String repwdTemp = Arrays.toString(tRepassword.getPassword());
+            if(pwdTemp.compareToIgnoreCase(repwdTemp)==0){
+                varUsername = tUserName1.getText();
+                varPassword = pwdTemp;
+                cl.next(pMain);
+            }else {
+                System.out.println("Password Not Match");
+            }
+
+        } else if  (e.getSource()==btnSignIn) {
+            String userTemp = tUserName2.getText();
+            String pwdTemp = Arrays.toString(tPassword2.getPassword());
+            if(attempt==1){
+                cl.show(pMain,"Incorrect");
+            }else {
+                if((varUsername.compareToIgnoreCase(userTemp)==0)&&(varPassword.compareToIgnoreCase(pwdTemp))==0){
+                    cl.show(pMain,"Correct");
+                }else {
+                    attempt--;
+                    lAttempt.setText(String.valueOf(attempt));
+                }
+            }
+
+
+        } else if (e.getSource()==btnClear) {
+            tUserName1.setText("");
+            tPassword.setText("");
+            tRepassword.setText("");
+        } else if (e.getSource()==btnClear2) {
+            tUserName2.setText("");
+            tPassword2.setText("");
         }
     }
 }
